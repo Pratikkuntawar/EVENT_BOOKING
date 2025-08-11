4b97123d763530c266c63d23bb8dbb5519b7c7cf
ConsultAdd <consultadd@ConsultAdds-MacBook-Pro.local>
2025-08-12 00:11:38 +0530

    small chnage in code architecture

--- backend/eventbooking/urls.py
+++ backend/eventbooking/urls.py
@@ -19,8 +19,8 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/users/', include('users.urls')),
-    # path('api/events/', include('events.urls')),
-    # path('api/booking/', include('bookings.urls')),
+    path('api/users/', include('users.urls')),
+    path('api/events/', include('events.urls')),
+    path('api/booking/', include('bookings.urls')),
 
 ]
